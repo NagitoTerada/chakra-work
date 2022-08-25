aec7b73102d534188417e3f9fa2965cc2b7c17f3
nagito.terada <nagito.terada@hokuden-it.co.jp>
2022-08-26 02:49:52 +0900

    表示はできた

--- packages/web/src/pages/Main/Main.tsx
+++ packages/web/src/pages/Main/Main.tsx
@@ -1,6 +1,35 @@
 import { Box } from "@chakra-ui/react";
-import React from "react";
+import moment from "moment";
+import React, { useMemo } from "react";
+import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 export const Main: React.FC = () => {
-  return <Box>this is Box component.</Box>;
+  return (
+    <>
+      <Box>this is Box component.</Box>
+      <Box sx={{ height: "600" }}>
+        <Calendar />
+      </Box>
+    </>
+  );
+};
+
+const Calendar = () => {
+  const { defaultDate } = useMemo(
+    () => ({
+      defaultDate: new Date(),
+    }),
+    []
+  );
+
+  return (
+    <BigCalendar
+      localizer={momentLocalizer(moment)}
+      startAccessor="start"
+      endAccessor="end"
+      defaultDate={defaultDate}
+      style={{ height: 500 }}
+    />
+  );
 };
