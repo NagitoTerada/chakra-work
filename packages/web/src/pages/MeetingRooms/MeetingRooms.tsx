fa2c67d7525c8a78062d7856d2c02aa068cf9ff4
nagito.terada <nagito.terada@hokuden-it.co.jp>
2022-09-15 21:35:56 +0900

    コメントアウト削除

--- packages/web/src/pages/MeetingRooms/MeetingRooms.tsx
+++ packages/web/src/pages/MeetingRooms/MeetingRooms.tsx
@@ -1,5 +1,8 @@
 import { Box, Text, VStack } from "@chakra-ui/react";
 
+// TODO 取得するまでのテスト変数
+const roomList = ["A会議室", "B会議室", "C会議室"];
+
 export const MeetingRooms: React.FC = () => {
   return (
     <VStack>
@@ -15,33 +18,3 @@ export const MeetingRooms: React.FC = () => {
     </VStack>
   );
 };
-
-const roomList = ["A会議室", "B会議室", "C会議室"];
-
-/**
- * 
- *   {data?.roomList?.map((room) => (
-        <Box
-          key={room?.dataValue}
-          w="100%"
-          borderColor="GrayText"
-          borderRadius="5px"
-          p={2}
-          backgroundColor="gray.100"
-          onClick={() => onClick(room as Room)}
-          as="button"
-        >
-          <HStack spacing={5} mx={2}>
-            <Text color="gray.600">{room?.floor}</Text>
-            <Stack spacing={1}>
-              <Text fontSize="xl" as="b" color="teal.900">
-                {room?.room}
-              </Text>
-              <Text color="gray.600" align="left">
-                {room?.capacity}名
-              </Text>
-            </Stack>
-          </HStack>
-        </Box>
-      ))}
- */
